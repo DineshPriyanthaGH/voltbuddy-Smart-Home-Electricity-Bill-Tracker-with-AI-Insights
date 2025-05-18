911cacda3889158c87ba121cd72edb83825eaa09
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-19 02:33:56 +0530

    EnergyEfficiencyTipsPage:page.js

---
+++ voltbuddy-frontend/app/EnergyEfficiencyTips/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EnergyEfficiencyTipsPage() {
+  return (
+    <div>
+      <h1>Energy Efficiency Tips</h1>
+      {/* Your content here */}
+    </div>
+  );
+}
