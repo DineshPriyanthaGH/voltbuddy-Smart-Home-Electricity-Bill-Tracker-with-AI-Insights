8178fc246cd2b3e713bb6b78da0f15ed8b21d85d
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-20 14:43:47 +0530

    smsService:parameters

--- voltbuddy-backend/services/smsService.js
+++ voltbuddy-backend/services/smsService.js
@@ -3,3 +3,10 @@ const twilio = require('twilio');
 const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 const VERIFY_SERVICE_SID = process.env.TWILIO_VERIFY_SERVICE_SID;
 
+/**
+ * Send OTP to the user's mobile number using Twilio Verify API
+ * @param {string} to - The recipient's mobile number
+ * @param {string} locale - The preferred locale (default: 'en')
+ * @returns {Promise<Object>} - Twilio API response
+ */
+
