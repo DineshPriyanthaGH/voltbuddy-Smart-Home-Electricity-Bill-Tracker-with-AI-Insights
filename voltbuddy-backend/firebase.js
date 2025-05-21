b1af3ffa74c65f3a4e554eee473b8e26f8ebc3c8
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-21 17:24:21 +0530

    Up:firebase auth

--- voltbuddy-backend/firebase.js
+++ voltbuddy-backend/firebase.js
@@ -1,12 +1,14 @@
 // firebase.js
 const admin = require('firebase-admin');
 
-// Initialize Firebase Admin SDK with your service account credentials
-const serviceAccount = require('./config/voltbuddy-firebase-adminsdk.json');  // Use the correct path to the JSON file
+if (!admin.apps.length) {
+  // Initialize Firebase Admin SDK with your service account credentials
+  const serviceAccount = require('./config/voltbuddy-firebase-adminsdk.json');  // Use the correct path to the JSON file
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const auth = admin.auth();  // Firebase authentication instance
 
