7d238c80e3ec8ee21b7d533c499564240a54af50
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-21 17:28:26 +0530

    firebase.js

---
+++ voltbuddy-frontend/firebase.js
@@ -0,0 +1,22 @@
+// firebase.js (Frontend)
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const auth = firebase.auth();
+export { auth };
