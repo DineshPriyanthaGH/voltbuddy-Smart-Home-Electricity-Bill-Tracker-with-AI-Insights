4afb0cf7f4d6da319f05b09e62a625e938dd61b4
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-21 17:28:57 +0530

    Authroutes.js

--- voltbuddy-backend/routes/authRoutes.js
+++ voltbuddy-backend/routes/authRoutes.js
@@ -1,12 +1,12 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const { requestOtp, verifyOtp } = require('../controllers/authController');  // Import the functions
+const { requestOtp, verifyOtp } = require('../controllers/authControllers');  // Import the functions
 
 // Route for sending OTP to the mobile number
 router.post('/request-otp', requestOtp);  // Endpoint to request OTP
 
-// Route for verifying the OTP entered by the user
+//node  Route for verifying the OTP entered by the user
 router.post('/verify-otp', verifyOtp);  // Endpoint to verify OTP
 
 module.exports = router;
