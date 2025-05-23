821ce5407efcdefa26a930a0752b432388bcfb3f
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-23 09:05:57 +0530

    Auth:user.js

--- voltbuddy-backend/models/User.js
+++ voltbuddy-backend/models/User.js
@@ -1,12 +1,20 @@
 // models/User.js
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  mobileNumber: {
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
     required: true,
-    unique: true,
-    match: [/^(?:\+94|0)?[0-9]{9}$/, 'Invalid Sri Lankan mobile number format']
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
   },
   appliances: [
     {
