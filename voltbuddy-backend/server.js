03163ffc69a9d29c9060ccdf253e5fc58e10c7f3
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-24 07:43:59 +0530

    SERVER.JS

--- voltbuddy-backend/server.js
+++ voltbuddy-backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRouter = require('./routers/authRouter');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
@@ -29,6 +29,10 @@ app.use('/api/auth', authRouter);
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
+
+const dashboardRouter = require('./routes/dashboardRoutes');
+
+app.use('/api/dashboard', dashboardRouter);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
