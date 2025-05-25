00daa432cf672584c2cf8b6a5000c6746a6fef55
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-05-25 21:46:12 +0530

    update models

--- voltbuddy-backend/models/OtpSession.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const OtpSessionSchema = new mongoose.Schema({
-  mobileNumber: { type: String, required: true },
-  otpCode: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
-});
-
-module.exports = mongoose.model('OtpSession', OtpSessionSchema);
