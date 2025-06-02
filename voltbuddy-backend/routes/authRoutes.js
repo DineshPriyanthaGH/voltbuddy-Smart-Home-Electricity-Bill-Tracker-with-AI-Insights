299dee27ed695c013b2245dadee047becf48aa71
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-02 10:52:02 +0530

    authroutes.js

--- voltbuddy-backend/routes/authRoutes.js
+++ voltbuddy-backend/routes/authRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authControllers');
 
-// POST for registration
+
 router.post('/register', register);
 
-// GET for login (as requested)
 router.post('/login', login);
 
 module.exports = router;
