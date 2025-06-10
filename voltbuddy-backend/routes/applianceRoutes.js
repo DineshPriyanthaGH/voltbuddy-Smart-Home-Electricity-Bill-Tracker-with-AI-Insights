0ff19bb12fb07af67b1fd3ef9c127e7627094c69
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-06-11 01:46:31 +0530

    Up:applianceroutes

--- voltbuddy-backend/routes/applianceRoutes.js
+++ voltbuddy-backend/routes/applianceRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const applianceController = require('../controllers/applianceController');
-const authMiddleware = require('../middleware/authMiddleware'); 
+const { authMiddleware } = require('../middleware/authMiddleware'); 
 
 
 router.use(authMiddleware);
