51adb96e5e49564db85397051a1092f545658491
Dinesh Priyantha <Priyanthadinesh2002@gmail.com>
2025-07-16 11:54:03 +0530

    energytips routes

--- voltbuddy-backend/routes/energyTipsRoutes.js
+++ voltbuddy-backend/routes/energyTipsRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { generateEnergyTips, generateCostStrategies } = require('../controllers/energyTipsController');
+const { generateEnergyTips, generateCostStrategies,generatePredictions } = require('../controllers/energyTipsController');
 const router = express.Router();
+const { authMiddleware } = require('../middleware/authMiddleware');
 
 router.post('/', generateEnergyTips);
 router.post('/cost-strategies', generateCostStrategies);
+router.post('/predictions', authMiddleware, generatePredictions);
 
 module.exports = router;
